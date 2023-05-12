6e0a8760f1756529f4c040a07554d5aa7887bd12
CruzHeitor <113639149+CruzHeitor@users.noreply.github.com>
2023-05-12 17:02:41 +0000

    feat: projeto inicial

---
+++ my-app/src/App.tsx
@@ -0,0 +1,268 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <h1>
+          Tarsila's Birthday!
+        </h1>
+        <p>Essa experiência fica melhor com música!</p>
+
+        <iframe style={{ margin: 12 }} src="https://open.spotify.com/embed/playlist/4d5JXbDwRESZYDM2Mr758X?utm_source=generator&theme=0" width="60%" height="352" frameBorder="0" allowFullScreen={false} allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+
+        <video src="./Download.mp4" controls />
+        <p>
+          Esse é somente um dos vídeos que fazem parte da nossa história.
+          <br></br>O musical.ly foi uma parte muito importante para a nossa amizade, já que passávamos o dia inteiro lá kkkkkkkkkkkkkkkk
+          <br></br>
+
+
+          <img src='./tarsila5.jpg' alt="Tarsila 5" width={"80%"} />
+          <img src='./tarsila1.jpg' alt="Tarsila 1" width={"80%"} />
+          <img src='./tarsila2.jpg' alt="Tarsila 2" width={"80%"} />
+          <img src='./tarsila3.JPG' alt="Tarsila 3" width={"80%"} />
+          <img src='./tarsila4.JPG' alt="Tarsila 4" width={"80%"} />
+
+          <p> <img src='./tarsila6.jpg' alt="Tarsila 6" width={"80%"} /> </p>
+
+          <br></br> Eu nunca fiz site então se esse ficar estranho me perdoa kkkkkkkk
+          <br></br> Vou deixar agora alguns depoimentos dos seus queridos amigos!
+        </p>
+
+        <h1>AMIGOS S2</h1>
+
+        <p>
+          Acho que não tem pessoa melhor para começar os depoimentos do que sua própria mamãe!<br></br>
+        </p>
+
+        <div className="row">
+          <div className="column">
+            "Feliz aniversário, amor da minha vida!!
+            Que você continue crescendo/amadurecendo cercada do amor de seus amigos leais.
+            Sou grata por viver ao seu lado. Receba as bênçãos de prosperidade vindas do Senhor.
+            Eu te amo e te honro."
+            <br></br>~Elma
+          </div>
+
+          <div className="column">
+
+            <div className='img-edit'>
+
+              <div className="side-image">
+                <img src='./tarsilapais.jpg' alt="Tarsila Pais" width={"80%"} />
+              </div>
+              <div className='side-image'>
+                <img src='./tarsilamae.jpg' alt="Tarsila Mae" width={"80%"} />
+              </div>
+              <div className='side-image'>
+                <img src='./tarsilamae2.jpg' alt="Tarsila Mae 2" width={"80%"} />
+              </div>
+              <div className='side-image'>
+                <img src='./tarsilamae3.jpg' alt="Tarsila Mae 3" width={"80%"} />
+              </div>
+
+            </div>
+          </div>
+
+          <p>
+            <img src='./tarsilakid.jpg' alt="Tarsila Kid" width={"60%"} />
+          </p>
+          <br></br>
+        </div>
+
+        <div className="row">
+          <div className="column">
+            "Feliz aniversário, TARSI!! Fico muito feliz de participar de mais um ano na sua vida e poder ver o quanto você brilha cada vez mais.
+            Obrigada por ser quem é: um ser cheio de amor e de carinho, que sempre esteve e está do meu lado.
+            Felicidades nesse seu dia maravilhoso, assim como você.
+            Te amo demais, e agradeço a Deus por nossos destinos terem se cruzado e, dessa forma, poder ser sua amiga, te abraçar, compartilhar experiências e momentos incríveis com você.
+            Te amo! Feliz dia 💜✨"
+            <br></br>~Cecis
+          </div>
+
+          <div className="column">
+
+            <div className='img-edit'>
+
+              <div className="side-image">
+                <img src='./tarsicecis1.jpg' alt="Tarsila Cecis 1" width={"80%"} />
+              </div>
+              <div className='side-image'>
+                <img src='./tarsicecis2.jpg' alt="Tarsila Cecis 2" width={"80%"} />
+              </div>
+              <div className='side-image'>
+                <img src='./tarsicecis3.jpg' alt="Tarsila Cecis 3" width={"80%"} />
+              </div>
+              <div className='side-image'>
+                <img src='./tarsicecis4.jpg' alt="Tarsila Cecis 4" width={"80%"} />
+              </div>
+
+            </div>
+          </div>
+
+          <br></br>
+          <br></br>
+
+          <p>
+            <img src='./tarsilakid2.jpg' alt="Tarsila Kid 2" width={"60%"} />
+          </p>
+          <br></br>
+
+        </div>
+
+        <div className="row">
+          <div className="column">
+            "Feliz aniversário, Tarsila!!! 🥳🎉
+            Que Deus continue te abençoando e iluminando seu caminho!
+            Não apenas neste dia em particular, mas todos os dias, eu te desejo um lindo dia cheio de amor e alegria!
+            Te desejo toda felicidade do mundo, tudo de bom nessa nova etapa da sua vida, e muitos anos de vida! Felicidades sempre!!! ❤️✨🎂🫶🏻"
+            <br></br>~Tyff
+          </div>
+
+          <div className="column">
+
+            <div className='img-edit'>
+
+              <div className="side-image">
+                <img src='./tarsilagalera.jpg' alt="Tarsila Galera" width={"80%"} />
+              </div>
+              <div className='side-image'>
+                <img src='./tarsilagalera2.jpg' alt="Tarsila Galera 2" width={"80%"} />
+              </div>
+              <div className='side-image'>
+                <img src='./tarsilagalera3.jpg' alt="Tarsila Galera 3" width={"80%"} />
+              </div>
+              <div className='side-image'>
+                <img src='./tarsilagalera4.jpg' alt="Tarsila Galera 4" width={"80%"} />
+              </div>
+
+            </div>
+          </div>
+
+
+          <br></br>
+          <br></br>
+
+          <p>
+            <img src='./tarsilakid3.jpg' alt="Tarsila Kid 3" width={"60%"} />
+          </p>
+          <br></br>
+
+        </div>
+
+        <div className="row">
+          <div className="column">
+            "Eii Tatá, senta aí que lá vem retrospectiva!
+            Bom como de costume dos velhos tempos vulgo 8°ano é sempre bom relembrar datas importante, pois todo ano você faz aniversário mas nem todo ano é o aniversário é o mesmo. Isso se dá ao fator incrivel e as vezes dificil de entender chamado VIDA.
+            E ela por si só nos faz ser feliz, viver momentos de profunda tristeza, mas momentos de extrema alegria também e posso dizer que é isso que nossa amizade trás de especial, poder viver esses momentos juntas. Saber que independente do momento estaremos ali e por mais dificil e pesado que esteja tudo quando nos reunimos é certeza que iremos comer muito chips KKKKKK beber refri e CANTAR HORRORES todos os hits atemporais!!!
+            Tatá depois dessa falação toda que arrumei só quero que você se sinta abraçada e acolhida pelo amor da nossa amizade KKKKKK desde os tempos do vôlei até agora que estamos perdidas nesse mundão! OBRIGADA PELA SUA VIDA E AMO VOCÊ!!! Feliz aniversário !!!!!! 13.05.2023"
+            <br></br>~Lice
+          </div>
+
+          <div className="column">
+
+            <div className='img-edit'>
+
+              <div className="side-image">
+                <img src='./tarsilaalice.jpg' alt="Tarsila Alice" width={"80%"} />
+              </div>
+              <div className='side-image'>
+                <img src='./tarsilaalice2.jpg' alt="Tarsila Alice 2" width={"80%"} />
+              </div>
+              <div className='side-image'>
+                <img src='./tarsilaalice3.JPG' alt="Tarsila Alice 3" width={"80%"} />
+              </div>
+              <div className='side-image'>
+                <img src='./tarsilaalice4.jpg' alt="Tarsila Alice 4" width={"80%"} />
+              </div>
+
+            </div>
+          </div>
+
+          <br></br>
+          <br></br>
+
+          <p>
+            <img src='./tarsilakid4.jpg' alt="Tarsila Kid 4" width={"60%"} />
+          </p>
+          <br></br>
+
+        </div>
+
+        <div className="row">
+          <div className="column">
+            "TEXTOTEXTO ABREUTEXTO ABREUTEXTO ABREUTEXTO ABREUTEXTO ABREUTEXTO ABREUTEXTO ABREUTEXTO ABREUTEXTO ABREUTEXTO ABREUTEXTO
+            ABREUTEXTO ABREUTEXTO ABREUTEXTO ABREUTEXTO ABREUTEXTO ABREUTEXTO ABREUTEXTO ABREUTEXTO ABREUTEXTO ABREU ABREUU"
+            <br></br>~Abreu
+          </div>
+
+          <div className="column">
+
+            <div className='img-edit'>
+
+              <div className="side-image">
+                <img src='./tarsilaabreu.jpg' alt="Tarsila Abreu" width={"80%"} />
+              </div>
+              <div className='side-image'>
+                <img src='./tarsilaalice2.jpg' alt="Tarsila Abreu 2" width={"80%"} />
+              </div>
+              <div className='side-image'>
+                <img src='./tarsilaalice3.JPG' alt="Tarsila Abreu 3" width={"80%"} />
+              </div>
+              <div className='side-image'>
+                <img src='./tarsilaalice4.jpg' alt="Tarsila Abreu 4" width={"80%"} />
+              </div>
+
+            </div>
+          </div>
+
+          <br></br>
+          <br></br>
+
+          <p>
+            <img src='./tarsilakid5.jpg' alt="Tarsila Kid 5" width={"60%"} />
+          </p>
+          <br></br>
+
+        </div>
+
+        <div className="row">
+          <p>
+            Feliz aniversário, Tarsilaaaaaaaaaaaa!!!! Depois de diversas linhas de código, várias fotos baixadas e muitos erros na programação eu estou aqui.
+            Te desejo um feliz aniversário e que Deus te abençoe grandemente, você não faz ideia do quão especial você é para mim! Agora se prepara para o textão.
+            Enquanto eu fazia esse site/app não sei exatamente o que será no final eu fui reunindo fotos, músicas, VÍDEOS e tantas outras coisas que me faziam lembrar de tudo o que passamos nesses anos,
+            criar essa playlist não foi fácil porque eu toda hora colocava uma música a mais e voltava no tempo e revivia tudo mais uma vez!
+            Sinto que essas músicas me fazem voltar em tempos mais simples onde ficávamos jogando Minecraft o dia inteiro ouvindo Ateez kkkkkkkk
+            Voltei nas conversas do discord, nas nossas discussões sobre o que construir, como pegar os materiais e tantas outras coisas que me marcaram profundamente.
+            Foi e é um prazer crescer e estar dividindo a vida com você! Você se tornou uma mulher incrível (sempre foi), cheia de personalidade e de identidade.
+            Eu te amo muuuuuuuuuiitooooo, nós somos incrivelmente parecidos e eu acho isso incrível! Amei que do nada voltamos a se falar diariamente com áudios gigantescos cheios de risadas sobre uma música qualquer do "Mateus" kkkkkkkkkkk.
+            Espero que você esteja aproveitando o seu dia, que tenha comido s'mores e cantado bastante comigo, te amo muito! Te desejo tudo de bom s2.
+            ~Heitor eboy
+          </p>
+          <br></br>
+
+          <img src='./tarsilaheitor.jpg' alt="Tarsila Heitor" width={"60%"} />
+
+          <img src='./tarsilaheitor2.JPG' alt="Tarsila Heitor 2" width={"60%"} />
+
+          <img src='./tarsilaheitor3.jpg' alt="Tarsila Heitor 3" width={"60%"} />
+
+          <img src='./tarsilaheitor4.jpg' alt="Tarsila Heitor 4" width={"60%"} />
+
+          
+
+
+        </div>
+
+      </header>
+
+    </div>
+  );
+}
+
+export default App;
